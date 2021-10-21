073edc2c00edd3763803e5ec4f60eb2abf252af3
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2021-10-21 13:21:53 -0400

    live class: click-change works

--- cart211-class-live/script.js
+++ cart211-class-live/script.js
@@ -1,7 +1,35 @@
-let myVariable = `<img id="dynamicPicture" src="assets/arrow.png">`;
+const soundFile = new Audio("assets/sound.wav");
+let lastTitle = "Variable Title";
+let lastImage = "assets/camerons-world.png";
+let imagePosition = 0;
+
+//Function called every 20 millis.
+setInterval(movePicture, 20);
 
 function myfunction() {
   // document.getElementById("dynamicHeader").innerHTML = "New Color";
-  document.getElementById("dynamicHeader").innerHTML = myVariable;
-  document.getElementById("dynamicPicture").style.display = "inline";
+  soundFile.play();
+
+//swapImage is the action.
+//DyanmicPicture = old image, lastImage = new image
+  let swapImage = lastImage;
+  lastImage = document.getElementById("dynamicPicture").src;
+  document.getElementById("dynamicPicture").src = swapImage;
+
+  // document.getElementById("dynamicPicture").style.display = "inline";
+  document.getElementById("dynamicHeader").style.backgroundColor = "blue";
+
+  let swapTitle = lastTitle;
+  lastTitle = document.getElementById("dynamicHeader").innerHTML;
+  document.getElementById("dynamicHeader").innerHTML = swapTitle;
+}
+
+function movePicture() {
+  if (imagePosition === 300) {
+      clearInterval(movePicture);
+  } else {
+    //imagePosition = imagePosition + 1;
+    imagePosition = imagePosition + 3;
+    document.getElementById("dynamicPicture").style.right = imagePosition + "px";
+  }
 }
