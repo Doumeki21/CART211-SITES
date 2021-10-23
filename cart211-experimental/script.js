4653b6c9d75422dff2c4f1eea291863220b240ec
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2021-10-22 23:59:58 -0400

    not working, taking a break

---
+++ cart211-experimental/script.js
@@ -0,0 +1,16 @@
+// $(window).on("load", function(){
+//   $(".loader-wrapper").fadeOut("slow");
+// });
+
+// function toggleMenu() {
+//   const menuToggle = document.querySelector('.toggle');
+//   menuToggle.classList.toggle('active')
+// }
+
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+}
+
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+}
