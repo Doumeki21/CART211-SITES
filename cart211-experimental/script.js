a1b7b37dd9d2c49fb2e436fbb4c3ee191a67c8c3
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2021-10-24 14:28:35 -0400

    experimented on typewriter effect, changing navbar, rgba, anchor tag

--- cart211-experimental/script.js
+++ cart211-experimental/script.js
@@ -1,16 +1,26 @@
-// $(window).on("load", function(){
-//   $(".loader-wrapper").fadeOut("slow");
-// });
 
-// function toggleMenu() {
-//   const menuToggle = document.querySelector('.toggle');
-//   menuToggle.classList.toggle('active')
-// }
 
-function w3_open() {
-  document.getElementById("mySidebar").style.display = "block";
-}
+window.onload = function() {
+  console.log("running");
+  setTimeout(loadIndex, 5000);
 
-function w3_close() {
-  document.getElementById("mySidebar").style.display = "none";
+  let text = ['W','e','l','c','o','m','e'];
+  let currentIndex =0;
+  let typeWritten ="";
+  let typeAnimation = setInterval(typeWelcome,500);
+
+  function loadIndex() {
+    document.getElementById("loader-wrapper").style.display = "none";
+    document.getElementById("index-wrapper").style.display = "block";
+  }
+
+  function typeWelcome(){
+    typeWritten+=text[currentIndex];
+    console.log(typeWritten);
+    document.getElementById("welcome-text").textContent = typeWritten;
+    currentIndex++;
+    if(currentIndex>=text.length){
+      clearInterval(typeAnimation);
+    }
+  }
 }
