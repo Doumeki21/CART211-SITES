dbf32353fc56be61d54d464ff1278479efe5ba43
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2021-10-30 16:03:38 -0400

    fixed so that custom cursor works directly below cursor!

--- cart211-experimental/index.js
+++ cart211-experimental/index.js
@@ -46,7 +46,7 @@ window.onload = function() {
     let mouse_offset_x = event.clientX - pBox.x;
     let mouse_offset_y = event.clientY - pBox.y;
 
-    document.getElementById("moveMe").style.left = (mouse_offset_x - mBox.width / 2) + "px";
-    document.getElementById("moveMe").style.top = (mouse_offset_y - mBox.height / 2) + "px";
+    document.getElementById("moveMe").style.left = (mouse_offset_x - mBox.width / 2) + 10 + "px";
+    document.getElementById("moveMe").style.top = (mouse_offset_y - mBox.height / 2) + 10 + "px";
   }
 }
