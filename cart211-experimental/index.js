ae5e7c464927b3371678521c9f0397a5d3cda8a1
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2021-10-31 15:45:44 -0400

    fixed cursor on other pages, changed link color, contact page format

--- cart211-experimental/index.js
+++ cart211-experimental/index.js
@@ -1,12 +1,12 @@
 window.onload = function() {
   //Loader screen
   console.log("running");
-  setTimeout(loadIndex, 5000);
+  setTimeout(loadIndex, 3000);
 
   let text = ['W', 'e', 'l', 'c', 'o', 'm', 'e'];
   let currentIndex = 0;
   let typeWritten = "";
-  let typeAnimation = setInterval(typeWelcome, 500);
+  let typeAnimation = setInterval(typeWelcome, 200);
 
   function loadIndex() {
     document.getElementById("loader-wrapper").style.display = "none";
@@ -21,33 +21,6 @@ window.onload = function() {
     if (currentIndex >= text.length) {
       clearInterval(typeAnimation);
     }
-  }
-
-  //CURSOR (CANNOT BE IN ANOTHER JS FILE!! NEEDS TO BE INSIDE INDEX.JS)
-  //2: put on the parent;
-  document.getElementById("parent").addEventListener("mousemove", mouseCapture);
-
-  function mouseCapture() {
-
-    ///1:
-    // Can only work when circle is absolute!
-    // document.getElementById("moveMe").style.left = event.clientX+"px";
-    // document.getElementById("moveMe").style.top = event.clientY+"px";
-
-    //2:: put in center
-    let mBox = document.getElementById("moveMe").getBoundingClientRect();
-    // console.log(mBox);
-
-    // //3:: need the parent offset::
-    //     //NOTE that mouseEVent coordinates are ALWAYS relative to the window ...
-    //getBoundingClientRect becomes parent here!
-    let pBox = this.getBoundingClientRect();
-    //Here we calculate the offset depending where the parent is!!
-    let mouse_offset_x = event.clientX - pBox.x;
-    let mouse_offset_y = event.clientY - pBox.y;
-
-    document.getElementById("moveMe").style.left = (mouse_offset_x - mBox.width / 2) + 1 + "px";
-    document.getElementById("moveMe").style.top = (mouse_offset_y - mBox.height / 2) + 1 + "px";
   }
 }
 
