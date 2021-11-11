9e175139f3c5c9f640053fa415c4336e961be2b3
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2021-11-11 13:03:54 -0500

    updated class live

--- cart211-class-live/script.js
+++ cart211-class-live/script.js
@@ -4,12 +4,53 @@ let lastImage = "assets/camerons-world.png";
 let imageHorizontal = 0;
 let imageVertical = 0;
 
+let dragHorizontal = 0;
+let dragVertical = 0;
+let dragOffsetH = 0;
+let dragOffsetV = 0;
+let dragging = false;
+
+
+  dragHorizontal = window.localStorage.getItem("dragHorizontal");
+  dragVertical = window.localStorage.getItem("dragVertical");
+
+function onLoad() {
+  document.getElementById("dragImage").style.left = dragHorizontal - 100 + "px";
+  document.getElementById("dragImage").style.top = dragVertical - 50 + "px";
+}
+
+
 //Function called every 20 millis.
 // setInterval(movePicture, 60);
 
 //event are all identified by browser!!
 //call our keyboard function.
 document.addEventListener("keydown", keyboard);
+
+function mouseMove() {
+  if (dragging) {
+    dragHorizontal = event.pageX;
+    dragVertical = event.pageY;
+
+
+    document.getElementById("dragImage").style.left = dragHorizontal - 100 + "px";
+    document.getElementById("dragImage").style.top = dragVertical - 50 + "px";
+  }
+}
+
+function mouseDown() {
+  dragOffsetH = event.pageX -parseInt(document.getElementById("dragImage").style.left, 10);
+    dragOffsetV = event.pageY - document.getElementById("dragImage").style.top;
+  dragging = true;
+}
+
+function mouseUp() {
+  dragging = false;
+
+//take variable into the local storage.
+  window.localStorage.setItem("dragHorizontal", dragHorizontal);
+  window.localStorage.setItem("dragVertical", dragVertical);
+}
 
 function myfunction() {
   // document.getElementById("dynamicHeader").innerHTML = "New Color";
@@ -29,14 +70,10 @@ function myfunction() {
   document.getElementById("dynamicHeader").innerHTML = swapTitle;
 }
 
-function movePicture()
-{
-  if (imageHorizontal === 300)
-  {
+function movePicture() {
+  if (imageHorizontal === 300) {
     clearInterval(movePicture);
-  }
-  else
-  {
+  } else {
     //imagePosition = imagePosition + 1;
     imageHorizontal = imageHorizontal + 3;
     //Offset = left
@@ -44,20 +81,18 @@ function movePicture()
   }
 }
 
-function keyboard(event)
-{
+function keyboard(event) {
   console.log(imageHorizontal);
 
-  if(event.keyCode === 38) {
+  if (event.keyCode === 38) {
     imageVertical -= 3;
-  }
-  else if (event.keyCode === 40) {
+  } else if (event.keyCode === 40) {
     imageVertical += 3;
   } //left key
-  else if(event.keyCode === 37 && imageHorizontal <= 300) {
+  else if (event.keyCode === 37 && imageHorizontal <= 300) {
     imageHorizontal -= 3;
   } //Right key
-  else if(event.keyCode === 39) {
+  else if (event.keyCode === 39) {
     imageHorizontal += 3;
   }
   //When A is pressed-
@@ -67,12 +102,12 @@ function keyboard(event)
 
     for (let i = 0; i < array.length; i++) {
       //The first thing in the class should turn this color.
-          array[i].style.color = "#FF55FF";
+      array[i].style.color = "#FF55FF";
     }
   }
-  if(imageHorizontal <= -100) {
-      window.alert("you did it!");
+  if (imageHorizontal <= -100) {
+    window.alert("you did it!");
   }
   document.getElementById("dynamicPicture").style.left = imageHorizontal + "px";
-  document.getElementById("dynamicPicture").style.bottom = imageVertical + "px";
+  document.getElementById("dynamicPicture").style.top = imageVertical + "px";
 }
