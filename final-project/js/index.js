908ce4dd9c20f840094b09d814170cecae71c67c
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2021-11-14 20:48:04 -0500

    p2: yay pop up works and fixed button alignment with float

--- final-project/js/index.js
+++ final-project/js/index.js
@@ -26,7 +26,9 @@ window.onload = function() {
 
 function toggle() {
   var blur = document.getElementById("blur");
-  blur.classList.toggle("active")
+  blur.classList.toggle("active");
+  var popup = document.getElementById("popup");
+  popup.classList.toggle("active");
 }
 
 // //Smooth scroll with anchor tag
