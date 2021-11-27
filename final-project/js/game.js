fbd7c4c8ec19e81a7cfd6f9c5febb07feca4bd0c
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2021-11-27 17:04:09 -0500

    final site: game works!, fixed typo on html

--- final-project/js/game.js
+++ final-project/js/game.js
@@ -32,20 +32,106 @@ class MixOrMatch {
     this.timeRemaining = this.totalTime;
     this.matchedCards = [];
     this.busy = true;
-  }
 
+    //waits 500ms before doing wats inside the function
+    //the setTimeout goes smoother with the states (startGame, gameover).
+    setTimeout(() => {
+      this.shuffleCards();
+      this.countDown = this.startCountDown();
+      this.busy = false;
+    }, 500);
+    this.hideCards();
+    //Restart the timer and flips
+    this.timer.innerText = this.timeRemaining;
+    this.ticker.innerText = this.totalClicks;
+  }
+  hideCards() {
+    this.cardsArray.forEach(card => {
+      card.classList.remove(`visible`);
+      card.classList.remove(`matched`);
+    });
+  }
   flipCard(card) {
     if (this.canFlipCard(card)) {
       this.audioController.flip();
       this.totalClicks++;
       this.ticker.innerText = this.totalClicks;
+      //card flips
+      card.classList.add(`visible`);
+
+      if (this.cardToCheck)
+        //check for match
+        this.checkForCardMatch(card);
+      else
+        this.cardToCheck = card;
     }
   }
+  checkForCardMatch(card) {
+    if (this.getCardType(card) === this.getCardType(this.cardToCheck))
+      this.cardMatch(card, this.cardToCheck);
+    //match
+    else
+      this.cardMisMatch(card, this.cardToCheck);
+
+    this.cardToCheck = null;
+  }
+  cardMatch(card1, card2) {
+    this.matchedCards.push(card1);
+    this.matchedCards.push(card2);
+    card1.classList.add(`matched`);
+    card2.classList.add(`matched`);
+    this.audioController.match();
+    if (this.matchedCards.length === this.cardsArray.length)
+      this.victory();
+  }
+  cardMisMatch(card1, card2) {
+    this.busy = true;
+    setTimeout(() => {
+      card1.classList.remove(`visible`);
+      card2.classList.remove(`visible`);
+      this.busy = false;
+    }, 1000);
+  }
+  getCardType(card) {
+    return card.getElementsByClassName(`card-value`)[0].src;
+  }
+
+  startCountDown() {
+    return setInterval(() => {
+      this.timeRemaining--;
+      this.timer.innerText = this.timeRemaining;
+      if (this.timeRemaining === 0)
+        this.gameOver();
+    }, 1000);
+  }
+  gameOver() {
+    //clears countDown.
+    clearInterval(this.countDown);
+    // this.audioController.gameOver();
+    //shows gameOver screen.
+    document.getElementById(`game-over-text`).classList.add(`visible`);
+  }
+  victory() {
+    clearInterval(this.countDown);
+    document.getElementById(`victory-text`).classList.add(`visible`);
+  }
+
+  //CREATING A SHUFFLE ALGORITHM USING VANILLA JS!!
+  shuffleCards() {
+    //create a backwards for loop
+    for (let i = this.cardsArray.length - 1; i > 0; i--) {
+      //Math.random creates a number b/w 0 and 1 (not including 1)
+      let randIndex = Math.floor(Math.random() * (i + 1));
+      //we shuffle based on the css grid, order property. rather than the array.
+      this.cardsArray[randIndex].style.order = i;
+      this.cardsArray[i].style.order = randIndex;
+    }
+  }
+
   canFlipCard(card) {
     //cannot flip card when matched!
-    return true;
     //if all these statements are false, retun true.
-    // return !this.busy && !this.matchedCards.includes(card) && card !===this.cardToCheck;
+    return !this.busy && !this.matchedCards.includes(card) && card !== this.cardToCheck;
   }
 }
 
@@ -55,7 +141,7 @@ function ready() {
   //call all cards
   let cards = Array.from(document.getElementsByClassName(`card`));
   // 100 sec
-  let game = new MixOrMatch(100, cards);
+  let game = new MixOrMatch(50, cards);
 
   //forEach() is used with Arrays
   //foreach = takes a function to call for every item in the Array
